65467afdcf72a9dc2328e39de4ba2fa4381a7387
Slashscreen <SlashScreen@users.noreply.github.com>
2024-02-03 17:09:44 -0800

    Added span

--- src/hcompress.cpp
+++ src/hcompress.cpp
@@ -1,8 +1,46 @@
 #include "hcompress.hpp"
 
 using namespace HCompressor;
+using namespace godot;
 
-float **Compressor::read_image()
+float **Compressor::read_image(Image *img)
 {
-    float **arr = new float *[MAP_DIMENSIONS];
+    int w = img->get_width();
+    int h = img->get_height();
+    float **arr = new float *[w];
+
+    for (size_t x = 0; x < w; x++)
+    {
+        arr[x] = new float[h];
+
+        for (size_t y = 0; y < h; y++)
+        {
+            arr[x][y] = img->get_pixel(x, y).r; // Read red channel
+        }
+    }
+
+    return arr;
+}
+
+float **Compressor::grab_span(float ***data, AABB *aabb)
+{
+    float **arr = new float *[aabb->width];
+
+    for (size_t x = 0; x < aabb->width; x++)
+    {
+        arr[x] = new float[aabb->height];
+
+        for (size_t y = 0; y < aabb->height; y++)
+        {
+            arr[x][y] = *data[x][y]; // simply copying the data is I Think less bytes than using pointers
+        }
+    }
+
+    return arr;
+}
+
+// public
+
+void Compressor::process_maps(TypedArray<Image> imgs)
+{
 }
--- src/hcompress.hpp
+++ src/hcompress.hpp
@@ -1,11 +1,17 @@
 #pragma once
 #include "constants.hpp"
+#include "aabb.hpp"
 #include <godot_cpp/variant/typed_array.hpp>
+#include <godot_cpp/classes/image.hpp>
 
 namespace HCompressor
 {
     class Compressor
     {
-        float **read_image();
+        float **read_image(godot::Image *img); // Get heightmap array from image
+        float **grab_span(float ***data, AABB *aabb);
+
+    public:
+        void process_maps(godot::TypedArray<godot::Image> imgs);
     };
 }
